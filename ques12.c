abb12708bec8d458d855facdad47bc9697a956a1
Suraj bhatt <110667958+Surajbhatt2003@users.noreply.github.com>
2022-10-22 10:46:24 +0530

    Update ques12.c

--- ques12.c
+++ ques12.c
@@ -1,1 +1,14 @@
-will write in future
+ #include <stdio.h>
+
+int main()
+{            int a,num,b,reverse;
+    printf(" enter 3 digit number");
+ scanf("%d",&a);
+     reverse = a%10;
+     b = a/10;
+     num= reverse*100 +b;
+ 
+  printf("  right sifting  is: %d",num);
+    return 0;
+}
+
